6a6d6372b6c13e71044ba52f20d4bc07fb4b84b3
Hasbi Alfaridzi <52277749+HasbiAlfaridzi@users.noreply.github.com>
2021-12-23 18:00:19 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as covband} from './app.json';
-
-AppRegistry.registerComponent(covband, () => App);
